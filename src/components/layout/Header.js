32736d5bed9ef77432a44f7dfa300d6d9a848811
Alexorac <alexandre@ledeuc.fr>
2024-02-20 18:49:54 +0100

    mise en place

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,14 +6,11 @@ function Header() {
     return (
         <header>
             <div className="logo-container">
-                <img src="path_to_your_logo" alt="Logo" className="logo" />
+                <img src="path_to_your_image" alt="Image du logo" />
             </div>
             <nav className="menu">
                 <ul>
                     <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/about">À propos</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/featured-products">Produits Phares</Link></li>
                 </ul>
             </nav>
